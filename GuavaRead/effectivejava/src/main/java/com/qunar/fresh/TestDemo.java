48feed7e416570603e26d73c6ca862caf3df1cec
junhuahan <hanjunhua.123123@gmail.com>
2017-04-22 13:55:03 +0800

    test

--- GuavaRead/effectivejava/src/main/java/com/qunar/fresh/TestDemo.java
+++ GuavaRead/effectivejava/src/main/java/com/qunar/fresh/TestDemo.java
@@ -1,7 +1,14 @@
 package com.qunar.fresh;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Version: 1.0.0 Date:2017-04-22 Time: 13:51 Author: junhua.han <br>
  */
 public class TestDemo {
+    private static final Logger logger = LoggerFactory.getLogger(TestDemo.class);
+    public static void main(String[] args) {
+        logger.info("hello");
+    }
 }
