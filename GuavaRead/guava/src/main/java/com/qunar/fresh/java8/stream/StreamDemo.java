8771e4d7bbc69fd3a08b42a83834ffe67a81a10c
junhua.han <junhua.han@qunar.com>
2017-12-01 10:55:55 +0800

    java8

--- GuavaRead/guava/src/main/java/com/qunar/fresh/java8/stream/StreamDemo.java
+++ GuavaRead/guava/src/main/java/com/qunar/fresh/java8/stream/StreamDemo.java
@@ -1,8 +1,10 @@
 package com.qunar.fresh.java8.stream;
 
+import static com.sun.tools.doclint.Entity.sum;
 import static java.util.stream.Collectors.toList;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.google.common.collect.Lists;
 import com.google.gson.Gson;
@@ -14,6 +16,7 @@ import com.google.gson.GsonBuilder;
 public class StreamDemo {
 
     private static Gson gson = new GsonBuilder().create();
+
     public static void main(String[] args) {
         List<Integer> list = Lists.newArrayList(1, 2, 3, 4, 5);
         List<Integer> collect = list.stream().map(i -> i * i).collect(toList());
@@ -33,7 +36,31 @@ public class StreamDemo {
 
         System.out.println(gson.toJson(collect2));
 
+        if (numbers1.stream().anyMatch(i -> i > 2)) {
+            System.out.println("has match");
+        }
 
+        if (numbers1.stream().allMatch(i -> i > 0)) {
+            System.out.println("all match");
+        }
 
+        if (numbers1.stream().noneMatch(i -> i > 100)) {
+            System.out.println("none match");
+        }
+
+        Optional<Integer> any = numbers1.stream().filter(i -> i > 2).findAny();
+        System.out.println(any.isPresent() ? any.get() : "not exist");
+
+        Optional<Integer> first = numbers1.stream().filter(i -> i > 1).findFirst();
+        System.out.println(first.isPresent() ? first.get() : "not exists");
+
+        Optional<Integer> reduce = numbers1.stream().reduce((i, j) -> i + j);
+        System.out.println(gson.toJson(reduce));
+
+        Integer reduceWithInit = numbers1.stream().reduce(0, (i, j) -> i + j);
+        System.out.println(reduceWithInit);
+
+        Integer reduceClassSum = numbers1.stream().reduce(0, Integer::sum);
+        System.out.println(reduceClassSum);
     }
 }
